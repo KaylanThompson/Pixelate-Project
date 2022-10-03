120fbe5a37e43e8125bd9a5e11744b4f1917ef7a
timothy <timothybrennan8@gmail.com>
2022-10-03 14:26:10 -0400

    completed the switcing colors

--- script.js
+++ script.js
@@ -19,29 +19,37 @@ button.addEventListener("click", makeRow);
 
 table.addEventListener("click", colorize)
 
+
+
+ const color = document.getElementsByTagName("select")[0]
+ let chosenColor = "red"
+
+
+ color.addEventListener("change", function (event){
+    chosenColor = event.target.value
+ }) 
+
+
 function colorize (event){
    const target = event.target
    if (target.className.length) {
     target.className = ''
    }else {
-    target.className = 'red'
+    target.className = chosenColor
    }
-}
- 
-
-
- const color = document.getElementsByTagName("select")[0]
-
- color.addEventListener("change", function (event){
-    console.log(event.target.value);
-
- }) 
-
- 
-
+ }
 
  
 
 
 
-   
+
+
+
+
+
+
+
+
+
+ 
