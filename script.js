7e778d249d45749bd21b26fb1b2ce1c87d2e0006
timothy <timothybrennan8@gmail.com>
2022-10-03 12:02:29 -0400

    Finished Event Target  and now we can make art

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Your code here
-const table = document.getElementsByTagName("table")[0]
+let table = document.getElementsByTagName("table")[0];
 
-function makeRow (){
-    const row = document.createElement("tr")
-    for( let i = 0; i < 20; i++) {
-        const td = document.createElement('td')
-    row.appendChild(td)}
-
-    table.appendChild(row)   
-
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
 }
 
-makeRow()
-makeRow ()
-makeRow ()
-makeRow ()
-makeRow ()
-makeRow ()
-makeRow ()
+makeRow();
+
+const button = document.getElementById("add-row");
+button.addEventListener("click", makeRow);
 
 
-const button = document.getElementById("add-row")
 
+table.addEventListener("click", colorize)
+
+function colorize (event){
+   const target = event.target
+   if (target.className.length) {
+    target.className = ''
+   }else {
+    target.className = 'red'
+   }
+ }
+   
